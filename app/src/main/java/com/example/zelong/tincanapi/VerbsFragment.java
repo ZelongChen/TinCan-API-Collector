6acf602dfb574de0a1ec8414f403ac0262dd0536
ZelongCHEN <chen.zelong0814@gmail.com>
2016-03-02 18:46:18 +0100

    add restclient and download verbs

--- app/src/main/java/com/example/zelong/tincanapi/VerbsFragment.java
+++ app/src/main/java/com/example/zelong/tincanapi/VerbsFragment.java
@@ -3,14 +3,25 @@ package com.example.zelong.tincanapi;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.zelong.tincanapi.Tools.RestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class VerbsFragment extends Fragment {
 
 
@@ -18,12 +29,63 @@ public class VerbsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        downloadVerbs();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verbs, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_verbs, container, false);
+        listView = (ListView) view.findViewById(R.id.verbs_list);
 
+        return view;
+    }
+    ListView listView;
+    ArrayAdapter adapter;
+    ArrayList<String> verbs = new ArrayList();
+    private void downloadVerbs() {
+        RestClient.get("verbs", null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        verbs.add(response.getJSONObject(i).getString("display"));
+                    } catch (JSONException e) {
+                        Log.d("JSON Exception", "onSuccess: " + e.getLocalizedMessage());
+                    }
+                }
+                adapter = new ArrayAdapter(getActivity(), android.R.layout.activity_list_item, verbs.toArray());
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                super.onSuccess(statusCode, headers, responseString);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
 }
